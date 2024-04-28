3f5854f818a7a9409e006a11832a5b495a48c577
Zaidan <zai@Colossus>
2024-04-28 15:36:13 +0800

    Quartz sync: Apr 28, 2024, 3:36 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,9 +6,11 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const logo = `https://${cfg.baseUrl}/static/vault.svg`
   return (
     <h1 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>{title}</a>
+      <img src={logo} alt="the vault" width="310" height="210"></img>
     </h1>
   )
 }
